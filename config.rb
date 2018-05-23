d82561dd95f3c1fa908342edbb8bdc45a9721b68
takashi.isobe <issobero153@gmail.com>
2018-05-24 03:38:11 +0900

    2018/05/24

--- config.rb
+++ config.rb
@@ -10,7 +10,9 @@ javascripts_dir = "../app/assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+# output_style = :expanded
+# output_style = (environment == :production) ? :compressed : :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
